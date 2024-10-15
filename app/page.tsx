3ee3c6f4e539559d37e7958df9ca13c019818a41
tanmay.patil <tanmay.patil@datastack.tech>
2024-10-15 17:27:09 +0530

    dsPane

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,10 @@
 import Image from "next/image";
 // import styles from "./page.module.css";
  import styles from "./Components/DsButton/Ds_Button.module.css";
+ import PaneStyles from "./Components/DsPane/DsPane.module.css";
 import addIcon from "./Icons/add.svg";
 import DSButton from "./Components/DsButton/DsButton";
+import DsPane from "./Components/DsPane/DsPane";
 
 export default function Home() {
   // const handleMouseHover = (e) => {
@@ -19,7 +21,8 @@ export default function Home() {
   //   console.log("Action button clicked");
   // };
  
-  return (
+  return (<>
+    <DsPane type="ClosePane" side={PaneStyles.right}></DsPane>
     <div className={styles.btn}>
       <DSButton
         id="actionBtn"
@@ -28,7 +31,21 @@ export default function Home() {
         // handleOnHover={handleMouseHover}
         beforeIcon={<Image className="add" src={addIcon} alt="Add Icon" />}
         buttonText="New"
-      />
+        />
     </div>
+      <DsPane type="ClosePane" side={PaneStyles.left}>
+      <div className={styles.btn}>
+      <DSButton
+        id="actionBtn"
+        buttonClass={styles.action_btn}
+        // handleOnClick={handleActionClick}
+        // handleOnHover={handleMouseHover}
+        beforeIcon={<Image className="add" src={addIcon} alt="Add Icon" />}
+        buttonText="New"
+        />
+    </div>
+      </DsPane>
+    
+        </>
   );
 }
