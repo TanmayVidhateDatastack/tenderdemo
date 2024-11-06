5628d049df04f96fcdaec524ff5d12bc67defdd4
akshata.bachche <akshata.bachche@datastack.tech>
2024-11-06 11:42:18 +0000

    dropdown with context updated

--- app/page.tsx
+++ app/page.tsx
@@ -1,15 +1,10 @@
-
+import DemoSelect from "./Components/dsSelect/dsDemoSelect";
 import Demo from "./ElProComponents/Demo/demo";
 
 export default function Home() {
   return (
     <>
-      <Demo></Demo>
-
-    <DemoSummaryCount></DemoSummaryCount>
-    <DemoDeviation></DemoDeviation>
-    <DemoUserProfile></DemoUserProfile>
+      <DemoSelect></DemoSelect>
     </>
-
   );
 }
