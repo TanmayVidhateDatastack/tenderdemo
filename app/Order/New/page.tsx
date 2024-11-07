f1178ae6eb08ae88834eb25e021fe8059e4f5d80
tanmay.patil <tanmay.patil@datastack.tech>
2024-11-07 16:34:05 +0530

    SO

--- app/Order/New/page.tsx
+++ app/Order/New/page.tsx
@@ -1,10 +1,19 @@
-import NavBack from '../../ElProComponents/NavigationComponent/navBack';
-
+import NavBack from "../../ElProComponents/NavigationComponent/navBack";
+import styles from "../order.module.css";
 
 export default function Home() {
   return (
     <>
-    <NavBack>Back</NavBack>
+      <NavBack>Back</NavBack>
+      <div className={styles.container}>
+        <div className={styles.Customer}>
+          <div className={styles.title}></div>
+          <div className={styles.inputDetails}></div>
+          <div className={styles.details}></div>
+        </div>
+        <div className={styles.Product}></div>
+        <div className={styles.footer}></div>
+      </div>
     </>
   );
 }
