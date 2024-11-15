cbb1d592e89c67cabf38ac9a1dea130c0d736e7e
GirishKhedikarDatastack <girish.khedikar@datastack.tech>
2024-11-15 14:43:12 +0530

    table updation

--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,14 @@
 import Demo from "@/app/Elements/ElProComponents/Demo/demo";
 import NavTo from "@/app/Elements/ElProComponents/NavigationComponent/navTo";
 
-
 export default function Home() {
   return (
     <>
-     <NavTo location="Order/New">New</NavTo>
-     <NavTo location="Order/Cancelation">Cancelation</NavTo>
-     <Demo></Demo>
+      <Demo></Demo>
+
+      <NavTo location="Order/New">New</NavTo>
+      <NavTo location="Order/Cancelation">Cancelation</NavTo>
+      <NavTo location="EwayBill/Bill">EwayBill</NavTo>
     </>
   );
 }
