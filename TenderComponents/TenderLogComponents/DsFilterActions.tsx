937963f61c97ef2ed9132e350cc48f85d21472fa
Kalyani Deshmukh <Kalyani.deshmukh@datastack.tech>
2025-03-22 16:01:22 +0530

    Tender with the setting the object

--- TenderComponents/TenderLogComponents/DsFilterActions.tsx
+++ TenderComponents/TenderLogComponents/DsFilterActions.tsx
@@ -1,12 +1,11 @@
 import styles from "./filteractions.module.css";
 import Image from "next/image";
 import filter from "@/Icons/smallIcons/filtericon.svg";
-import PaneOpenButton from "@/Elements/DsComponents/DsPane/PaneOpenButton";
 import { useAppDispatch, useAppSelector } from "@/Redux/hook/hook";
 import { AppDispatch, RootState } from "@/Redux/store/store";
 import fetchData from "@/helpers/Method/fetchData";
 import { Dispatch, SetStateAction, useEffect, useState } from "react";
-import { DsStatus, getTenderUserRoles } from "@/helpers/constant";
+import { dsStatus, DsStatus, getTenderUserRoles } from "@/helpers/constant";
 import { setUserRole } from "@/Redux/slice/UserSlice/userSlice";
 import { setVisibilityByRole } from "@/Redux/slice/PermissionSlice/permissionSlice";
 import { Tender } from "@/helpers/types";
@@ -20,14 +19,20 @@ import btnStyles from "@/Elements/DsComponents/DsButtons/dsButton.module.css";
 export interface DsFilterActionProps {
   data: Tender[];
   setFilteredData: Dispatch<SetStateAction<Tender[]>>;
+  searchQuery: string;
+  setSearchQuery: Dispatch<SetStateAction<string>>;
+  selectedStatus: string;
+  setSelectedStatus: Dispatch<SetStateAction<string>>;
 }
 const DsFilterActions: React.FC<DsFilterActionProps> = ({
   data,
   setFilteredData,
+  searchQuery,
+  setSearchQuery, selectedStatus, setSelectedStatus 
 }) => {
 
   const initialFilterState = Object.fromEntries(
-    [...Object.values(DsStatus), "nearSubmission", "feesPending"].map(
+    [...Object.values(DsStatus), "nearSubmission", "feesPending","feesPaid"].map(
       (status) => [status, false]
     )
   );
@@ -38,6 +43,7 @@ const DsFilterActions: React.FC<DsFilterActionProps> = ({
   const dispatch = useAppDispatch<AppDispatch>();
   const role = useAppSelector((state: RootState) => state.user.role);
   const permissions = useAppSelector((state: RootState) => state.permissions);
+  const [searchText, setSearchText] = useState("");
 
   const {
     tenderDatalistVisible,
@@ -69,107 +75,111 @@ const DsFilterActions: React.FC<DsFilterActionProps> = ({
       console.error("Fetch error: ", error);
     }
   };
+    useEffect(() => {
+    handleFetch();
+  }, [role]);
 
-  const handleFilter = (value: string | string[], message?: string) => {
-    setIsFiltered((prev) => {
-      const newFilterState = Object.fromEntries(
-        Object.keys(prev).map((key) => [
-          key,
-          Array.isArray(value)
-            ? value.includes(key)
-            : key === value
-            ? !prev[key]
-            : false,
-        ])
-      );
 
-      if (!Array.isArray(value)) {
-        if (!newFilterState[value]) {
-          setFilteredData(data);
-        } else {
-          let filteredRows = [...data];
+  // const handleFilter = (value: string | string[], message?: string) => {
+  //   setIsFiltered((prev) => {
+  //     const newFilterState = Object.fromEntries(
+  //       Object.keys(prev).map((key) => [
+  //         key,
+  //         Array.isArray(value)
+  //           ? value.includes(key)
+  //           : key === value
+  //           ? !prev[key]
+  //           : false,
+  //       ])
+  //     );
 
-          if (value === "nearSubmission") {
-            filteredRows = filteredRows.filter((tender) => {
+  //     if (!Array.isArray(value)) {
+  //       if (!newFilterState[value]) {
+  //         setFilteredData(data);
+  //       } else {
+  //         let filteredRows = [...data];
+
+  //         if (value === "nearSubmission") {
+  //           filteredRows = filteredRows.filter((tender) => {
      
-              const [day, month, year] = tender.submittionDate.split("/");
-              const dateToCheck = new Date(
-                parseInt(year),
-                parseInt(month) - 1,
-                parseInt(day)
-              );
+  //             const [day, month, year] = tender.submittionDate.split("/");
+  //             const dateToCheck = new Date(
+  //               parseInt(year),
+  //               parseInt(month) - 1,
+  //               parseInt(day)
+  //             );
 
-              const today = new Date();
-              const futureDate = new Date(today);
-              futureDate.setDate(today.getDate() + 20); //near submission change 20 days 
+  //             const today = new Date();
+  //             const futureDate = new Date(today);
+  //             futureDate.setDate(today.getDate() + 20); //near submission change 20 days 
 
     
-              return (
-                (dateToCheck < today || dateToCheck <= futureDate) &&
-                tender.status.tenderStatus?.toLowerCase() !== DsStatus.SMBT
-              );
-            });
-          } else if (value == "feesPending") {
-            const lowerCaseValue = DsStatus.DRFT.toLowerCase();
-            message = "fees pending";
-            filteredRows = filteredRows.filter((tender) =>
-              message
-                ? tender.status?.tenderStatus?.toLowerCase() ===
-                    lowerCaseValue &&
-                  tender?.status?.message?.toLowerCase() ==
-                    message.toLowerCase()
-                : tender.status?.tenderStatus?.toLowerCase() === lowerCaseValue
-            );
-          } 
+  //             return (
+  //               (dateToCheck < today || dateToCheck <= futureDate) &&
+  //               tender.status.tenderStatus?.toLowerCase() !== DsStatus.SMBT
+  //             );
+  //           });
+  //         } else if (value == "feesPending") {
+  //           const lowerCaseValue = DsStatus.DRFT.toLowerCase();
+  //           message = "fees pending";
+  //           filteredRows = filteredRows.filter((tender) =>
+  //             message
+  //               ? tender.status?.tenderStatus?.toLowerCase() ===
+  //                   lowerCaseValue &&
+  //                 tender?.status?.message?.toLowerCase() ==
+  //                   message.toLowerCase()
+  //               : tender.status?.tenderStatus?.toLowerCase() === lowerCaseValue
+  //           );
+  //         } 
      
-          else {
-            const lowerCaseValue = value.toLowerCase();
-            filteredRows = filteredRows.filter((tender) =>
-              message
-                ? tender.status?.tenderStatus?.toLowerCase() ===
-                    lowerCaseValue &&
-                  tender?.status?.message?.toLowerCase() ==
-                    message.toLowerCase()
-                : tender.status?.tenderStatus?.toLowerCase() === lowerCaseValue
-            );
-          }
-          setFilteredData(filteredRows);
-        }
-      } else {
-          const activeFilters = Object.entries(newFilterState)
-          .filter(([_, isActive]) => isActive)
-          .map(([key]) => key);
-        if (activeFilters.length === 0) {
-          setFilteredData(data);
-        } else {
-          let filteredRows = [...data];
+  //         else {
+  //           const lowerCaseValue = value.toLowerCase();
+  //           filteredRows = filteredRows.filter((tender) =>
+  //             message
+  //               ? tender.status?.tenderStatus?.toLowerCase() ===
+  //                   lowerCaseValue &&
+  //                 tender?.status?.message?.toLowerCase() ==
+  //                   message.toLowerCase()
+  //               : tender.status?.tenderStatus?.toLowerCase() === lowerCaseValue
+  //           );
+  //         }
+  //         setFilteredData(filteredRows);
+  //       }
+  //     } else {
+  //         const activeFilters = Object.entries(newFilterState)
+  //         .filter(([_, isActive]) => isActive)
+  //         .map(([key]) => key);
+  //       if (activeFilters.length === 0) {
+  //         setFilteredData(data);
+  //       } else {
+  //         let filteredRows = [...data];
 
-          filteredRows = filteredRows.filter((tender) =>
-            activeFilters.some(
-              (status) =>
-                tender.status?.tenderStatus?.toLowerCase() ===
-                  status.toLowerCase() &&
-                (tender.status.message && message
-                  ? tender.status.message.toLowerCase() == message.toLowerCase()
-                  : true)
-            )
-          );
-          setFilteredData(filteredRows);
-        }
+  //         filteredRows = filteredRows.filter((tender) =>
+  //           activeFilters.some(
+  //             (status) =>
+  //               tender.status?.tenderStatus?.toLowerCase() ===
+  //                 status.toLowerCase() &&
+  //               (tender.status.message && message
+  //                 ? tender.status.message.toLowerCase() == message.toLowerCase()
+  //                 : true)
+  //           )
+  //         );
+  //         setFilteredData(filteredRows);
+  //       }
 
-        return newFilterState;
-      }
-      return newFilterState;
-    });
-  };
+  //       return newFilterState;
+  //     }
+  //     return newFilterState;
+  //   });
+  // };
 
-  useEffect(() => {
-    handleFetch();
-  }, []);
+  // useEffect(() => {
+  //   handleFetch();
+  // }, []);
 
-  useEffect(() => {
-    console.log("deta : ", data);
-  }, [data]);
+  // useEffect(() => {
+  //   console.log("deta : ", data);
+  // }, [data]);
 
   useEffect(() => {
     if (role && role !== "") {
@@ -177,55 +187,114 @@ const DsFilterActions: React.FC<DsFilterActionProps> = ({
     }
   }, [role]);
 
-  const [searchText, setSearchText] = useState("");
+  // const [searchText, setSearchText] = useState("");
 
-  const handleSearch = (e) => {
-    if (e.key === "Enter") {
-      const searchQuery = normalizeText(searchText);
+  // const handleSearch = (e) => {
+  //   if (e.key === "Enter") {
+  //     const searchQuery = normalizeText(searchText);
 
      
-      const searchableColumns: (keyof Tender)[] = [
-        "customerName",
-        "tenderId",
-        "value",
-      ];
+  //     const searchableColumns: (keyof Tender)[] = [
+  //       "customerName",
+  //       "tenderId",
+  //       "value",
+  //     ];
 
-      const filteredRows = data.filter((originalData) => {
-        return searchableColumns.some((column) => {
-          const value = originalData[column]; 
+  //     const filteredRows = data.filter((originalData) => {
+  //       return searchableColumns.some((column) => {
+  //         const value = originalData[column]; 
 
-          if (typeof value === "string" || typeof value === "number") {
-            return normalizeText(value).includes(searchQuery);
-          }
+  //         if (typeof value === "string" || typeof value === "number") {
+  //           return normalizeText(value).includes(searchQuery);
+  //         }
 
-          if (Array.isArray(value)) {
-            return value.some(
-              (item: any) =>
-                typeof item === "string" &&
-                normalizeText(item).includes(searchQuery)
-            );
-          }
+  //         if (Array.isArray(value)) {
+  //           return value.some(
+  //             (item: any) =>
+  //               typeof item === "string" &&
+  //               normalizeText(item).includes(searchQuery)
+  //           );
+  //         }
 
-          if (typeof value === "object" && value !== null) {
-            return searchInObject(value, searchQuery);
-          }
+  //         if (typeof value === "object" && value !== null) {
+  //           return searchInObject(value, searchQuery);
+  //         }
 
-          return false;
-        });
-      });
+  //         return false;
+  //       });
+  //     });
 
-      setFilteredData(filteredRows);
+  //     setFilteredData(filteredRows);
+  //   }
+  // };
+
+
+  // const normalizeText = (text: any): string => {
+  //   return typeof text === "string"
+  //     ? text.toLowerCase()
+  //     : text.toString().toLowerCase();
+  // };
+
+
+  // const searchInObject = (obj: any, query: string): boolean => {
+  //   return Object.values(obj).some((val) => {
+  //     if (typeof val === "string" || typeof val === "number") {
+  //       return normalizeText(val).includes(query);
+  //     }
+
+  //     if (typeof val === "object" && val !== null) {
+  //       return searchInObject(val, query);
+  //     }
+
+  //     return false;
+  //   });
+  // };
+
+ 
+  const handleFilter = async (value: string) => {
+    console.log("valueee", value);
+    setIsFiltered((prev) => {
+      const newFilterState = Object.fromEntries(
+        Object.keys(prev).map((key) => [key, key === value ? !prev[key] : false])
+      );
+
+      const isFilterActive = !newFilterState[value];
+
+      if (isFilterActive) {
+        // If the filter is deselected, reset the data
+        // setFilteredData(data);
+        setSelectedStatus("");
+      } else {
+        const lowerCaseValue = value.toUpperCase();
+        // const filteredRows = data.filter(
+        //   (order) => order.status?.orderStatus?.toUpperCase() === lowerCaseValue
+        // );
+        // setFilteredData(filteredRows);
+        setSelectedStatus(lowerCaseValue);
+      }
+
+      return newFilterState;
+    });
+  };
+  const handleSearch = (e) => {
+    if (e.key === "Enter") {
+
+      // 
+      const searchQueryLower = searchText.toLowerCase();
+     
+      setSearchQuery(searchQueryLower);
+     
     }
   };
 
-
   const normalizeText = (text: any): string => {
-    return typeof text === "string"
-      ? text.toLowerCase()
-      : text.toString().toLowerCase();
+    if (typeof text !== "string") {
+      return text.toString().toLowerCase();
+    }
+    return text.toLowerCase(); // Do not escape special characters like `/`, `,`
   };
 
-
+  // Helper function to search inside nested objects
   const searchInObject = (obj: any, query: string): boolean => {
     return Object.values(obj).some((val) => {
       if (typeof val === "string" || typeof val === "number") {
@@ -239,45 +308,30 @@ const DsFilterActions: React.FC<DsFilterActionProps> = ({
       return false;
     });
   };
-
-  
+  console.log("searchhhhh", searchText);
+  console.log("searchinobject",searchInObject);
 
   return (
     <>
    
       {tenderDatalistVisible && (
-        <DsTextField
-          label="Search Tenders by ID,Name & Value"
-          id="userSelect"
-          disable={false}
-          initialValue=""
-        
-          iconEnd={
-            <div
-              style={{
-                width: "1.125em",
-                height: "1.125em",
-                position: "relative",
-              }}
-            >
-              <Image
-                src={searchicon}
-                layout="fill"
-                objectFit="cover"
-                alt="searchicon"
-              />
-            </div>
-          }
-
-          containerClasses={styles.datalist}
-          // onChange={(e) => setSearchText(e.target.value)}
-          onKeyUp={(e) => handleSearch(e)}
-        />
+       <DsTextField
+       placeholder="Search Tender by Id, Name & Value"
+       id="userSelect"
+       disable={false}
+       initialValue=""
+       iconEnd={
+         <div style={{ width: "1.125em", height: "1.125em", position: "relative" }}>
+           <Image src={searchicon} layout="fill" objectFit="cover" alt="searchicon" />
+         </div>
+       }
+       containerClasses={styles.datalist}
+       onChange={(e) => setSearchText(e.target.value)}
+       onKeyUp={(e) => handleSearch(e)}
+     />
       )}
       <div className={styles.nav}>
         {nearSubmissionButtonVisible && (
-
-      
           <DsFilterButton
             id="approved"
             buttonColor="btnPrimary"
@@ -301,7 +355,7 @@ const DsFilterActions: React.FC<DsFilterActionProps> = ({
           buttonViewStyle={
             isFiltered["feesPending"] ? "btnContained" : "btnOutlined"
           }
-          onClick={() => handleFilter("feesPending")}
+          onClick={() =>(DsStatus.APRV, "fees pending")}
           label="Fees Pending"
         />
       )}
@@ -318,10 +372,8 @@ const DsFilterActions: React.FC<DsFilterActionProps> = ({
           }
 
           onClick={() =>
-            handleFilter(
-              [DsStatus.UREV, DsStatus.APRV, DsStatus.UAPR],
-              "fees paid"
-            )
+            handleFilter
+              (DsStatus.APRL)
           }
           label="Approval"
         />
--- TenderComponents/TenderLogComponents/TenderlogFloatingMenu.tsx
+++ TenderComponents/TenderLogComponents/TenderlogFloatingMenu.tsx
@@ -2,6 +2,13 @@
 import DsButton from "@/Elements/DsComponents/DsButtons/dsButton";
 import { FloatingMenu, displayTableMenu } from "@/Elements/DsComponents/FloatingMenu/dsFloatingMenu";
 import { useEffect, useState } from "react";
+import Image from "next/image";
+import cancle from "@/Icons/smallIcons/cancle.svg";
+import lost from "@/Icons/smallIcons/lost.svg";
+import awarded from "@/Icons/smallIcons/awarded.svg";
+ import version  from "@/Icons/smallIcons/V.svg";
+// import cancle from "@/Icons/smallIcons/cancle.svg";
+
 
 interface DsTenderTableFloatingMenuProps {
   e: React.MouseEvent<HTMLElement>;
@@ -20,15 +27,19 @@ export const DsTenderTableFloatingMenu: React.FC<DsTenderTableFloatingMenuProps>
   const [isSubmitVisible, setIsSubmitVisible] = useState(false);
 
   useEffect(() => {
-    if (statuscell === "DRAFT") {
+    if (statuscell === "CANCELLED" || statuscell === "TENDERLOST") {
+      setIsCancelBtnVisible(false);
+    } else {
       setIsCancelBtnVisible(true);
+    }
+  
+    if (statuscell === "Submit ") {
       setLostBtnVisible(true);
       setIsPartiallyAwardedBtnVisible(true);
       setIsAwardedBtnVisible(true);
       setIsNewVersionBtnVisible(true);
       setIsSubmitVisible(true);
     } else {
-      setIsCancelBtnVisible(false);
       setLostBtnVisible(false);
       setIsPartiallyAwardedBtnVisible(false);
       setIsAwardedBtnVisible(false);
@@ -36,6 +47,7 @@ export const DsTenderTableFloatingMenu: React.FC<DsTenderTableFloatingMenuProps>
       setIsSubmitVisible(false);
     }
   }, [statuscell]);
+  
   
   useEffect(() => {
     console.log("Mouse Event:", e);
@@ -51,22 +63,133 @@ export const DsTenderTableFloatingMenu: React.FC<DsTenderTableFloatingMenuProps>
     >
       <>
         {isCancelBtnVisible && (
-          <DsButton id="deleteBtn" buttonColor="btnWarning" buttonViewStyle="btnContained" label="Tender Cancelled" />
+          <DsButton id="deleteBtn" buttonColor="btnWarning" buttonViewStyle="btnContained" 
+          startIcon={
+            <div
+              style={{
+                width: "1.125em",
+                height: "1.125em",
+                position: "relative",
+              }}
+            >
+              <Image
+                src={cancle}
+                alt="Add Icon"
+                layout="fill"
+                objectFit="cover"
+              />
+            </div>
+          }
+          label="Tender Cancelled" />
         )}
         {isLostBtnVisible && (
-          <DsButton id="Signbtn" buttonColor="btnPrimary" buttonViewStyle="btnContained" label="Tender Lost" />
+          <DsButton id="Signbtn" buttonColor="btnPrimary" buttonViewStyle="btnContained" 
+          startIcon={
+            <div
+              style={{
+                width: "1.125em",
+                height: "1.125em",
+                position: "relative",
+              }}
+            >
+              <Image
+                src={lost}
+                alt="Add Icon"
+                layout="fill"
+                objectFit="cover"
+              />
+            </div>
+          }
+          
+          
+          label="Tender Lost" />
         )}
         {isPartiallyAwardedBtnVisible && (
-          <DsButton id="InvoiceBtn" buttonColor="btnPrimary" buttonViewStyle="btnContained" label="Partially Awarded" />
+          <DsButton id="InvoiceBtn"     buttonColor="btnPrimary"
+        buttonViewStyle="btnText"
+          startIcon={
+            <div
+              style={{
+                width: "1.125em",
+                height: "1.125em",
+                position: "relative",
+              }}
+            >
+              <Image
+                src={awarded}
+                alt="Add Icon"
+                layout="fill"
+                objectFit="cover"
+              />
+            </div>
+          }
+          
+          label="Partially Awarded" />
         )}
         {isAwardedBtnVisible && (
-          <DsButton id="packinglistBtn" buttonColor="btnPrimary" buttonViewStyle="btnContained" label="Awarded" />
+          <DsButton id="packinglistBtn"     buttonColor="btnPrimary"
+        buttonViewStyle="btnText"
+          startIcon={
+            <div
+              style={{
+                width: "1.125em",
+                height: "1.125em",
+                position: "relative",
+              }}
+            >
+              <Image
+                src={awarded}
+                alt="Add Icon"
+                layout="fill"
+                objectFit="cover"
+              />
+            </div>
+          }
+          
+          label="Awarded" />
         )}
         {isNewVersionBtnVisible && (
-          <DsButton id="BillBtn" buttonColor="btnPrimary" buttonViewStyle="btnContained" label="Create New Version" />
+          <DsButton id="BillBtn"     buttonColor="btnPrimary"
+        buttonViewStyle="btnText"
+          startIcon={
+            <div
+              style={{
+                width: "1.125em",
+                height: "1.125em",
+                position: "relative",
+              }}
+            >
+              <Image
+                src={version}
+                alt="Add Icon"
+                layout="fill"
+                objectFit="cover"
+              />
+            </div>
+          }
+          
+          label="Create New Version" />
         )}
         {isSubmitVisible && (
-          <DsButton id="submit" buttonColor="btnPrimary" buttonViewStyle="btnContained" label="Customer Submission Done" />
+          <DsButton id="submit"     buttonColor="btnPrimary"
+        buttonViewStyle="btnText"
+          startIcon={
+            <div
+              style={{
+                width: "1.125em",
+                height: "1.125em",
+                position: "relative",
+              }}
+            >
+              <Image
+                src={lost}
+                alt="Add Icon"
+                layout="fill"
+                objectFit="cover"
+              />
+            </div>
+          }
+          label="Customer Submission Done" />
         )}
       </>
     </FloatingMenu>
