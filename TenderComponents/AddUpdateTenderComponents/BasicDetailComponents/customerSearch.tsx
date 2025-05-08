688070d642f4ef42440c1453a3fa4b9fc0ecb9b3
tanmay.patil <tanmay.patil@datastack.tech>
2025-05-08 19:26:59 +0530

    customer search Disable functionality added

--- TenderComponents/AddUpdateTenderComponents/BasicDetailComponents/customerSearch.tsx
+++ TenderComponents/AddUpdateTenderComponents/BasicDetailComponents/customerSearch.tsx
@@ -3,11 +3,7 @@ import {
   searchCustomerURL,
   getAllCustomerLocationsURL,
 } from "@/Common/helpers/constant";
-import {
-  customer,
-  datalistOptions,
-  location,
-} from "@/Common/helpers/types";
+import { customer, datalistOptions, location } from "@/Common/helpers/types";
 import { Dispatch, SetStateAction, useState } from "react";
 import React from "react";
 import DsSearchComponent from "@/Elements/DsComponents/DsSearch/searchComponent";
@@ -39,88 +35,95 @@ export function areSearchCustomers(value: unknown): value is customer[] {
 const CustomerSearch: React.FC<{
   customer: string;
   orderData?: TenderData;
- updateTenderData?: (
+  disabled?: boolean;
+  updateTenderData?: (
     key:
       | keyof TenderData
       | `tenderDetails.${keyof TenderData["tenderDetails"]}`,
     value: string | number
   ) => void;
   setCustomerLocations?: Dispatch<SetStateAction<location[]>>;
-}> = React.memo(({ customer, updateTenderData, setCustomerLocations }) => {
-  const [customers, setCustomers] = useState<datalistOptions[]>();
-  const [selectedCustomer, setSelectedCustomer] = useState<number>();
-  const [selectedAddress, setSelectedAddress] = useState<string>(""); // Track selected address
+}> = React.memo(
+  ({ customer, disabled, updateTenderData, setCustomerLocations }) => {
+    const [customers, setCustomers] = useState<datalistOptions[]>();
+    const [selectedCustomer, setSelectedCustomer] = useState<number>();
+    const [selectedAddress, setSelectedAddress] = useState<string>(""); // Track selected address
     const permissions = useAppSelector((state: RootState) => state.permissions);
-  const {
-  disable
-    } = permissions;
-  async function setSelectedOptions(option: datalistOptions): Promise<void> {
-    const selectedCustomerId = Number(option.id);
-    setSelectedCustomer(selectedCustomerId);
-    if (updateTenderData) {
-      updateTenderData("customerId", selectedCustomerId);
-      updateTenderData("tenderDetails.customerName", option.attributes.name||"");
-    }
-    setSelectedAddress("");
+    const { disable } = permissions;
+    async function setSelectedOptions(option: datalistOptions): Promise<void> {
+      const selectedCustomerId = Number(option.id);
+      setSelectedCustomer(selectedCustomerId);
+      if (updateTenderData) {
+        updateTenderData("customerId", selectedCustomerId);
+        updateTenderData(
+          "tenderDetails.customerName",
+          option.attributes.name || ""
+        );
+      }
+      setSelectedAddress("");
 
-    try {
-      const response = await fetch(
-        `${getAllCustomerLocationsURL}${selectedCustomerId}`
-      );
-      const data = await response.json();
- 
-      if (data.code === 200 && Array.isArray(data.result)) {
-        const formattedAddresses: location[] = data.result.map((addr) => ({
-          id: addr.id, 
-          address1: addr.address1,
-          address2: addr.address2,
-          address3: addr.address3, 
-          address4: addr.address4,
-          city: addr.city,
-          state: addr.state,
-          pinCode: addr.pinCode,
-          isPrimary: addr.isPrimary === "Y",
-        }));
-        setCustomerLocations?.(formattedAddresses);
-      } else {
-        console.error("Invalid API response:", data);
+      try {
+        const response = await fetch(
+          `${getAllCustomerLocationsURL}${selectedCustomerId}`
+        );
+        const data = await response.json();
+
+        if (data.code === 200 && Array.isArray(data.result)) {
+          const formattedAddresses: location[] = data.result.map((addr) => ({
+            id: addr.id,
+            address1: addr.address1,
+            address2: addr.address2,
+            address3: addr.address3,
+            address4: addr.address4,
+            city: addr.city,
+            state: addr.state,
+            pinCode: addr.pinCode,
+            isPrimary: addr.isPrimary === "Y",
+          }));
+          setCustomerLocations?.(formattedAddresses);
+        } else {
+          console.error("Invalid API response:", data);
+          setCustomerLocations?.([]);
+        }
+      } catch (error) {
+        console.error("Error fetching customer details:", error);
         setCustomerLocations?.([]);
       }
-    } catch (error) {
-      console.error("Error fetching customer details:", error);
-      setCustomerLocations?.([]);
     }
-  }
 
-  function setOptions(values: unknown) {
-    if (
-      Array.isArray(values) &&
-      values.every((val) => val.id && val.name && val.code)
-    ) {
-      const customers: datalistOptions[] = values.map((x) => ({
-        id: x?.id?.toString(),
-        value: `${x.code.toUpperCase()} - ${x.name}`,
-        attributes: { "customer-id": x.id.toString() ,"name":x.name ,"code":x.code},
-      }));
-      setCustomers(customers);
+    function setOptions(values: unknown) {
+      if (
+        Array.isArray(values) &&
+        values.every((val) => val.id && val.name && val.code)
+      ) {
+        const customers: datalistOptions[] = values.map((x) => ({
+          id: x?.id?.toString(),
+          value: `${x.code.toUpperCase()} - ${x.name}`,
+          attributes: {
+            "customer-id": x.id.toString(),
+            name: x.name,
+            code: x.code,
+          },
+        }));
+        setCustomers(customers);
+      }
     }
-  }
 
-  return (
-    <DsSearchComponent
-      containerClasses={styles.fields}
-      disable={disable}
-      id="customerSearch"
-      initialValue={customer}
-      dataListId="customerSearchDatalist" 
-      label={"Search Customer"}
-      options={customers || undefined}
-      setOptions={setOptions}
-      setSearchUrl={(searchTerm: string) => searchCustomerURL + searchTerm}
-      setSelectedOption={setSelectedOptions}
-    />
-  );
-});
+    return (
+      <DsSearchComponent
+        containerClasses={styles.fields}
+        disable={disable || disabled}
+        id="customerSearch"
+        initialValue={customer}
+        dataListId="customerSearchDatalist"
+        label={"Search Customer"}
+        options={customers || undefined}
+        setOptions={setOptions}
+        setSearchUrl={(searchTerm: string) => searchCustomerURL + searchTerm}
+        setSelectedOption={setSelectedOptions}
+      />
+    );
+  }
+);
 
 export default CustomerSearch;
-  
